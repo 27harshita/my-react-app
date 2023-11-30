6522d07bd82f0b5bc7b1ef67675613d89e16eedc
27harshita <152500490+27harshita@users.noreply.github.com>
2023-11-30 18:51:10 +0530

    Add files via upload

---
+++ my-app/src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src="https://media.licdn.com/dms/image/D4E03AQE4w260RhsZmg/profile-displayphoto-shrink_800_800/0/1680609301000?e=1706745600&v=beta&t=d3laIDudBn3aQPXJ8MI0MTCLIaS52ieJSHaS_Q5cCoY" className="App-logo" alt="logo" />
+        <p>
+          Hi! I am Harshita. Click below link to know more about me.
+        </p>
+        <a
+          className="App-link"
+          href="https://www.linkedin.com/in/harshita-gehlot-ba9866270/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Click Me
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
